a32f025f3783bb64307335eb0bb87891e16fe03d
Federico Marchini <federico.marchini@accenture.com>
2020-06-28 11:18:42 -0300

    Altas y modificaciones, Sync con la DB, otros.

---
+++ artshop.lppa.equipo.3/artshop.lppa.equipo.3.DATA/Services/IDataService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace artshop.lppa.equipo._3.DATA.Services
+{
+
+    
+    /// <summary>
+    /// Patron CRUD
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IDataService<T>
+    {
+        List<ValidationResult> ValidateModel(T model);
+        List<T> Get(
+            Expression<Func<T, bool>> whereExpression = null, Func<IQueryable<T>,
+            IOrderedQueryable<T>> orderFunction = null,
+            string includeModels = "");
+        T GetById(int id);
+        T Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Delete(int id);
+
+    }
+}
