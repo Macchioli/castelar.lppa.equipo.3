e390493fc2ca11bd1872d7638d3cdadda573851f
Nicolás Macchioli <63123389+Macchioli@users.noreply.github.com>
2020-07-02 12:39:48 -0300

    dataBase_lectura_escrituraAM

---
+++ artshop.lppa.equipo.3/artshop.lppa.equipo.3.DATA/Model/Artist.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace artshop.lppa.equipo._3.DATA.Model
+{
+    public class Artist :IdentityBase
+    {
+
+        [Required]
+        [DisplayName("Nombre")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Apellido")]
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string LifeSpan { get; set; }
+
+        [DisplayName("País")]
+        [Required]
+        public string Country { get; set; }
+        [DisplayName("Descripción")]
+        [Required]
+        public string Description { get; set; }
+        [DisplayName("Total")]
+        public int TotalProducts { get; set; }
+
+        [NotMapped]
+        public string FullName
+        {
+            get
+            {
+                return this.FirstName + " " + this.LastName;
+            }
+        }
+        public virtual ICollection<Product> Product { get; set; }
+
+    }
+
+}
+
---
+++ artshop.lppa.equipo.3/artshop.lppa.equipo.3.DATA/Model/IdentityBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace artshop.lppa.equipo._3.DATA.Model
+{
+    public class IdentityBase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        [Required]
+        public DateTime ChangedOn { get; set; }
+
+        public string ChangedBy { get; set; }
+
+    }
+}
